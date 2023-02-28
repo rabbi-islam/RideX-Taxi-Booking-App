00f6ec79bac6bd18f41527991a5e8c484a4431e0
Rabbi <Rabbyislam7777@gmail.com>
2023-02-28 12:30:42 +0600

    splash screen added

---
+++ app/src/main/java/com/rabbi/ridex_taxibooking/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.rabbi.ridex_taxibooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //hide status bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+            }
+        },2000);
+    }
+}
