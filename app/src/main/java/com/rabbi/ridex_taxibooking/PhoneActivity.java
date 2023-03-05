ab1cd83786a6a24664c3e85ea623ce13b438e4b7
Rabbi <Rabbyislam7777@gmail.com>
2023-03-05 07:57:52 +0600

    verification are are coming in phone successfully but Captcha occurring, have to solve that

--- app/src/main/java/com/rabbi/ridex_taxibooking/PhoneActivity.java
+++ app/src/main/java/com/rabbi/ridex_taxibooking/PhoneActivity.java
@@ -1,9 +1,11 @@
 package com.rabbi.ridex_taxibooking;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.graphics.Color;
@@ -18,16 +20,25 @@ import com.google.android.gms.auth.api.credentials.CredentialsApi;
 import com.google.android.gms.auth.api.credentials.HintRequest;
 import com.google.android.gms.auth.api.identity.GetPhoneNumberHintIntentRequest;
 import com.google.android.gms.auth.api.identity.Identity;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.hbb20.CountryCodePicker;
 import com.rabbi.ridex_taxibooking.databinding.ActivityPhoneBinding;
+
+import java.util.concurrent.TimeUnit;
 
 public class PhoneActivity extends AppCompatActivity {
 
     ActivityPhoneBinding binding;
-    LinearLayout verificationLinearLayout;
     CountryCodePicker ccp;
     private static final int CREDENTIAL_PICKER_REQUEST =120 ;
     private String selected_country_code = "+880";
+    ProgressDialog pd;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    FirebaseAuth auth;
 
 
     @Override
@@ -35,6 +46,9 @@ public class PhoneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityPhoneBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        pd = new ProgressDialog(this);
+        auth = FirebaseAuth.getInstance();
 
         ccp = findViewById(R.id.countryCodePicker);
         ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
@@ -80,8 +94,62 @@ public class PhoneActivity extends AppCompatActivity {
                 binding.continueNumberButton.setCursorVisible(false);
                 binding.continueNumberButton.setKeyListener(null);
 
+                if (binding.editTextPhone.getText().toString().trim().isEmpty()){
+                    Toast.makeText(PhoneActivity.this, "phone number must not be empty!", Toast.LENGTH_SHORT).show();
+                }else if (binding.editTextPhone.getText().toString().trim().length() != 10){
+                    Toast.makeText(PhoneActivity.this, "Invalid phone number!", Toast.LENGTH_SHORT).show();
+                }else{
+                    otpSend();
+                }
+
             }
         });
+        binding.verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(PhoneActivity.this, binding.firstPinView.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void otpSend() {
+        pd.setTitle("Sending OTP");
+        pd.setMessage("A 6 digit verification code is sending to "+ selected_country_code+binding.editTextPhone.getText().toString().trim());
+        pd.setCanceledOnTouchOutside(false);
+        pd.show();
+
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                pd.dismiss();
+                Toast.makeText(PhoneActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+
+                pd.dismiss();
+
+            }
+        };
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(auth)
+                        .setPhoneNumber(selected_country_code+binding.editTextPhone.getText().toString().trim())       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
     }
 
     @Override
